c9ab91e953d92ea501e3e822905218d4503fcaf2
gabrielamelian <gabrielamelian@gmail.com>
2016-11-10 15:13:11 +1300

    Add message column to migrations

--- seeds/hillary.js
+++ seeds/hillary.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('hillary').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('hillary').insert({id: 1, nickname: 'gabita', votecount: 5, sentiment: 'negative'}),
+        knex('hillary').insert({id: 2, nickname: 'gabita', votecount: 34, sentiment: 'positive'}),
+        knex('hillary').insert({id: 3, nickname: 'james', votecount: 100, sentiment: 'negative'})
       ]);
     });
 };

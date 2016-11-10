5ccca585dc0222c5c07f45cd3022f67882500e26
gabrielamelian <gabrielamelian@gmail.com>
2016-11-10 14:58:37 +1300

    Completed migration files for three tables

--- migrations/20161110143031_trump-table-create.js
+++ migrations/20161110143031_trump-table-create.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  
-};
+  console.log('Creating Trump table')
+  return knex.schema.createTableIfNotExists('trump', function(table){
+    table.increments('id')
+    table.string('nickname')
+    table.integer('votecount')
+    table.string('sentiment')
+  })
+  console.log('Table has been created')
+}
 
 exports.down = function(knex, Promise) {
-  
-};
+  return knex.schema.dropTableIfExists('trump').then(function() {
+    console.log("Trump table removed");
+  })
+}

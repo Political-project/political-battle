51db09e7bbc1df7467c6f2f74a2f1b0be524d5c2
michael-lowe-nz <mikkeyfluttie@gmail.com>
2016-11-11 15:31:24 +1300

    Add link for each post to clinton/id ?

--- views/showPost.js
+++ views/showPost.js
@@ -1,10 +1,12 @@
 var h = require('hyperscript');
 
-module.exports = function(name, message) {
+module.exports = function(name, message, id) {
   return h('div.post', {},
+    h('a', {href:`/clinton/${id}`},
     h('h2', {class: "name"}, `${name}`),
     h('p', {class: "message"}, `${message}`),
     h('button', {class: "upVote"}, "upVote"),
     h('button', {class: "downVote"}, "downVote")
+    )
   )
 }
--- views/showSentiment.js
+++ views/showSentiment.js
@@ -1,5 +1,8 @@
 var h = require('hyperscript');
 
-module.exports = function(table) {
-  return h('h3', {id:"trumpPositive"}, `Positivity directed at Mr. Trump ${table}`)
+module.exports = function(table, person, positive, negative) {
+  return h('div.sentimentBox.post', {id: `${table}Sentiment`},
+    h('h3', {}, `Positivity directed at ${person}: ${positive}`),
+    h('h3', {}, `Negativity directed at ${person}: ${negative}`)
+  )
 }
